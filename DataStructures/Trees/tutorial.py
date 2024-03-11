243ccd37da0d1ba5360ee5faaee9a79b27a3f183
Kunjesh <kunjpanchal2414@gmail.com>
2024-03-11 20:36:27 +0530

    AVL Trees

--- DataStructures/Trees/tutorial.py
+++ DataStructures/Trees/tutorial.py
@@ -1,4 +1,4 @@
-from utils import Node, Tree, BinarySearchTree
+from utils import *
 
 # tree = Tree()
 
@@ -16,32 +16,65 @@ from utils import Node, Tree, BinarySearchTree
 
 # tree.printInorder(tree.root)
 
-bst = BinarySearchTree()
+# bst = BinarySearchTree()
 
-bst.insert(10)
-bst.insert(15)
-bst.insert(5)
-bst.insert(25)
-bst.insert(20)
+# bst.insert(10)
+# bst.insert(15)
+# bst.insert(5)
+# bst.insert(25)
+# bst.insert(20)
 
-print('Inorder Traversal of Tree : ')
-bst.printInorder(bst.root)
-print('')
+# print('Inorder Traversal of Tree : ')
+# bst.printInorder(bst.root)
+# print('')
 
-presence_of_25 = bst.isInTree(25)
-if presence_of_25:
-    print('25 is present')
-else:
-    print('25 is absent')
+# presence_of_25 = bst.isInTree(25)
+# if presence_of_25:
+#     print('25 is present')
+# else:
+#     print('25 is absent')
 
-presence_of_1000 = bst.isInTree(1000)
-if presence_of_1000:
-    print('1000 is present')
-else:
-    print('1000 is absent')
+# presence_of_1000 = bst.isInTree(1000)
+# if presence_of_1000:
+#     print('1000 is present')
+# else:
+#     print('1000 is absent')
 
-del_data = bst.deleteNode(bst.root, 25)
-print('Tree After Deletion : ')
-bst.printInorder(bst.root)
-print('')
+# del_data = bst.deleteNode(bst.root, 25)
+# print('Tree After Deletion : ')
+# bst.printInorder(bst.root)
+# print('')
 
+# avl = AVLTree()
+
+# avl.insert(data=9)
+# avl.insert(data=3)
+# avl.insert(data=15)
+# avl.insert(data=17)
+# avl.insert(5)
+# avl.insert(20)
+# avl.insert(25)
+# avl.printDetailsOf(avl.root)
+
+myTree = AVL_Tree() 
+root = None
+  
+root = myTree.insert(root, 10) 
+root = myTree.insert(root, 20) 
+root = myTree.insert(root, 30) 
+root = myTree.insert(root, 40) 
+root = myTree.insert(root, 50) 
+root = myTree.insert(root, 25) 
+  
+"""The constructed AVL Tree would be 
+            30 
+           /  \ 
+         20   40 
+        /  \     \ 
+       10  25    50"""
+  
+# Preorder Traversal 
+print("Preorder traversal of the", 
+      "constructed AVL tree is") 
+myTree.preOrder(root) 
+print() 
